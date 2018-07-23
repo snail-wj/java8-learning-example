40034b0cd06608c2a84f74acd2f42e974b5f4485
wj <2927592708@qq.com>
2018-07-23 22:18:42 +0800

    message commit

---
+++ src/main/java/com/example/java8learningexample/Java8LearningExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.java8learningexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LearningExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8LearningExampleApplication.class, args);
+	}
+}
